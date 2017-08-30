a32c6f29f1beaee564cdfa8bed8f2e10aabf2c57
Kaden Hurlimann <khurlimann001@student.butte.edu>
2017-08-30 01:30:28 +0000

    lab1.2 commit

---
+++ lab12/lab122.cpp
@@ -0,0 +1,18 @@
+
+//Created By: April Browne
+//Created On: 8/22/2016
+ 
+ #include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+     cout<<"Hello"<<"World!"<<endl<<"hello kaden";
+ }
+ 
+ //Identify the following parts of the program:
+ //   preprocessor directives
+ //   main function
+ //   comments
+ //update it so that it is human readable
+ //add a second output line that says "Hello YourName!"
