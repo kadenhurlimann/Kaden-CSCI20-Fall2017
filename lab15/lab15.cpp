351d2a3aca840672cfa6f11133c570d0c13d3c9f
Kaden Hurlimann <khurlimann001@student.butte.edu>
2017-09-08 01:47:58 +0000

    Lab1.2 Commit

--- lab15/lab15.cpp
+++ lab15/lab15.cpp
@@ -3,7 +3,7 @@
     //mad lib lab 15
     
 #include <iostream> 
-#include <string>
+#include <string>                      // allows string                                               
 using namespace std ;
     
 int main()
@@ -14,7 +14,7 @@ int main()
     string NOUN3 = "culture" ;
     string NOUN4 = "listen" ;
     string VERB = "talking" ;
-    string VerbIng1 = "Spanish" ;             // initializing variables
+    string VerbIng1 = "Spanish" ;     // initializing variables
     string LANGUAGE = "Spain" ;
     string PLACE = "used" ;
     string PastParticiple = "excited" ;
@@ -22,13 +22,13 @@ int main()
     string ForeignPhrase ="Adios" ;
     
     cout << "Please type words that match the description or word type when prompted." << endl ;
-    cout << "Do not use any puntaution or spaces, only letters." << endl ;           // explainging the prompt to user
+    cout << "Do not use any puntaution or spaces, only letters." << endl ;                           // explainging the prompt to user
     cout << "Noun" << endl ;
     cin >> NOUN1 ;
     cout << "Noun" << endl ;
     cin >> NOUN2 ;
     cout << "Noun" << endl ;
-    cin >> NOUN3 ;                                                                                 // prompting user for variables
+    cin >> NOUN3 ;                                           // prompting user for variables
     cout << "Noun" << endl ;
     cin >> NOUN4 ;
     cout << "Verb" << endl ;
@@ -46,7 +46,7 @@ int main()
     cout << "your favorite foreign word" << endl ;
     cin >> ForeignPhrase ;
     
-    // 
+    
     // I did not create this madlib.
     //
     // Although some would say learning a foreign [NOUN] is hard, what it really takes is a little bit of [NOUN]. 
@@ -66,7 +66,7 @@ int main()
     cout << "   Although some would say learning a foreign " << NOUN1 << " is hard, what it really takes is a little bit of " << NOUN2 << "." << endl ;
     cout << "Study the " << NOUN3 << ", " << VERB << " to the music and practice " << VerbIng1 << " with others." << endl ;
     cout << "If you're studying " << LANGUAGE << ", for example, you can watch movies set in " << PLACE << "." << endl ;
-    cout << "If you can, take the time to visit a country where the language is " << PastParticiple << "." << endl ;                                  // forming the paragraph
+    cout << "If you can, take the time to visit a country where the language is " << PastParticiple << "." << endl ;                                  // writing the content
     cout << "The people there will be " << EMOTION << " to meet someone who has taken an interest in their " << NOUN4 << "." << endl ;                 // and plugging in variables
     cout << "" << endl ;
     cout << ForeignPhrase << "!" << endl ;
