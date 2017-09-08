70294bcd2ef76f2129903a16143360a78e8614df
Kaden Hurlimann <khurlimann001@student.butte.edu>
2017-09-08 01:43:47 +0000

    Lab1.2 Commit

--- lab12/lab122.cpp
+++ lab12/lab122.cpp
@@ -3,11 +3,18 @@
 //Created On: 8/22/2016
  
  #include <iostream>
+ #include <string>
  using namespace std;
  
  int main()
  {
+  
+  string a;
+  string b;
+  
      cout<<"Hello"<<"World!"<<endl<<"hello kaden";
+     cin >> a >> b;
+     cout << endl << a << " " << b;
  }
  
  //Identify the following parts of the program:
